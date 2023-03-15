283262ae9c5d24c2b697f15e22a3ba2b7e4c0795
Lloydie123 <jhonlloydborlagdan8@gmail.com>
2023-03-15 10:19:28 +0800

    Add Changes

--- models/models.py
+++ models/models.py
@@ -4,9 +4,10 @@ from odoo import models, fields, api
 
 class wgcc_savings(models.Model):
     _name = 'wgcc_savings.wgcc_savings'
-
+    
+    employee_id = fields.Many2one('hr.employee', string="Employee ID No.")
     name = fields.Char(string="Name:")
-    search = fields.Char(string="Search:")
+    search_field = fields.Char(string="Search:")
     deposit_amount = fields.Float(string='Deposit Amount', digits=(10, 2))
 
     deposit_date = fields.Date(string='Deposit Date')
@@ -20,18 +21,25 @@ class wgcc_savings(models.Model):
     withdrawable_mode = fields.Char(string="Widthrawal Mode:")
     amount_to_be_widthrawn = fields.Float(string='Amount to be withdrawn: ', digits=(10, 2))
     withdrawal_slip_no = fields.Integer(string='Withdrawal Slip No.')
-    cash = fields.Char(string="Cash:")
+    cash = fields.Char(string="(Words)")
     date_of_widthrawal = fields.Date(string='Date Of Widthrawal:')
     is_atm = fields.Boolean(string="ATM")
     release_date = fields.Date(string='Release Date')
     is_cheque = fields.Boolean(string="Check")
     cheque_information = fields.Char("Cheque Information: ")
-    bank = fields.Char("Cheque Information: ")
+    bank = fields.Char("Bank: ")
     cheque_no = fields.Char("Cheque No: ")
-    prepared_by = fields.Char("Prepared By: ")
-    posted_by = fields.Char("Posted By: ")
-    cancelled_by = fields.Char("Cancelled By: ")
-    cancel_reason = fields.Char("Cancel Reason: ")
+    prepared_by = fields.Char()
+    posted_by = fields.Char()
+    cancelled_by = fields.Char()
+    cancel_reason = fields.Char()
+    
+    status= fields.Selection([
+        ('draft', 'Draft'),
+        ('released', 'Released'),
+        ('cancelled', 'Cancelled'),
+        
+    ], string='Status', readonly="True", default="draft")
     
     
     
